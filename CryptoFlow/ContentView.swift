c5cb9b474524dd947d6dc2576ab35acdc4834d6f
Higor <92187145+HigorCris21@users.noreply.github.com>
2024-06-05 18:20:44 -0300

    Initial commit

---
+++ CryptoFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoFlow
+//
+//  Created by Higor  Lo Castro on 05/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoFlow/CryptoFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoFlowApp.swift
+//  CryptoFlow
+//
+//  Created by Higor  Lo Castro on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
